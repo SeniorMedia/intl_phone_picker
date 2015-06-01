791724f8c69e58b6b9d07d145db5626dcf0200aa
Fabien Dobat <fabien.dobat@seniormedia.fr>
2015-06-01 10:57:31 +0200

    Fix input field type

--- lib/intl_phone_picker/view_helpers.rb
+++ lib/intl_phone_picker/view_helpers.rb
@@ -2,7 +2,7 @@ module IntlPhonePicker
   module ViewHelpers
 
     def intl_phone_input_tag(value = nil, language = 'us')
-      number_field_tag 'intl_phone_input_' + language, value, class: 'tel'
+      telephone_field_tag 'intl_phone_input_' + language, value, class: 'tel'
     end
 
   end
