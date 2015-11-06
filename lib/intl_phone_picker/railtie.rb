a135429e564585393188f1bd0ec8658d9606fdaa
Fabien Dobat <fabien.dobat@seniormedia.fr>
2015-11-06 09:39:04 +0100

    Fix gem not loaded (::Rails::Engine)

--- lib/intl_phone_picker/railtie.rb
+++ lib/intl_phone_picker/railtie.rb
@@ -2,6 +2,9 @@ require 'intl_phone_picker/view_helpers'
 require 'intl_phone_picker/activerecord_helpers'
 
 module IntlPhonePicker
+  class Engine < ::Rails::Engine
+  end
+
   class Railtie < Rails::Railtie
     initializer "intl_phone_picker.view_helpers" do
       ActionView::Base.send :include, ViewHelpers
